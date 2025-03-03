c59771653a5dd62231796b3b6dd85d4515e7eeb6
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-03 17:04:20 +0600

    feat: add spinner component

---
+++ client/src/components/spinner/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Spinner = () => {
+	return (
+		<div className="timer-container">
+			<div className="timer-loader"></div>
+		</div>
+	);
+};
+
+export default Spinner;
