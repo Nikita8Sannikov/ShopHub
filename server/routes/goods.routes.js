253dc91169dd0f8e3e645ca022873e870f655c0c
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-08 21:51:12 +0600

    feat: add goods routes (CRUD)

---
+++ server/routes/goods.routes.js
@@ -0,0 +1,113 @@
+import { Router } from "express";
+import Goods from "../models/Goods.js";
+
+const router = Router();
+
+// /api/goods/productslist
+router.get("/productslist", async (req, res) => {
+	try {
+		const goods = await Goods.find()
+
+		res.status(200).json(goods);
+	} catch (e) {
+		res.status(500).json({
+			message: "Smth wrong, try again",
+			error: e.message,
+		});
+	}
+});
+
+export default router;
+
+// /api/goods/:id
+router.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const good = await Goods.findById(id);
+
+		if (!good) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+		res.status(200).json(good);
+	} catch (e) {
+		res.status(500).json({
+			message: "Smth wrong, try again",
+			error: e.message,
+		});
+	}
+});
+
+// /api/goods/:id
+router.patch("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const {title, category, description, price, rating, image} = req.body;
+
+		const good = await Goods.findById(id);
+		if (!good) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+		(good.title = title),
+			(good.category = category),
+			(good.avatar = avatar),
+			(good.description = description),
+			(good.price = price),
+			(good.rating = rating),
+			(good.image = image),
+			
+			await good.save();
+		res.status(200).json(good);
+	} catch (e) {
+		res.status(500).json({
+			message: "Smth wrong, try again",
+			error: e.message,
+		});
+	}
+});
+
+// /api/goods/:id
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const good = await Goods.findById(id);
+        if (!good) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+        await good.remove();
+        res.status(200).json({ message: "Good deleted" });
+    } catch (e) {
+        res.status(500).json({
+            message: "Smth wrong, try again",
+            error: e.message,
+        });
+    }
+});
+
+// /api/goods/create
+router.post("/create", async (req, res) => {
+    try {
+        const {title, category, description, price, rating, image} = req.body;
+
+        const good = new Goods({
+            title,
+            category,
+            description,
+            price,
+            rating,
+            image
+        });
+        await good.save();
+        res.status(201).json(good);
+    } catch (e) {
+        res.status(500).json({
+            message: "Smth wrong, try again",
+            error: e.message,
+        });
+    }
+});
