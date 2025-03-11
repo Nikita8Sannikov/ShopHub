8758a3d1b2dcca781c931ce757f1618c50c77374
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-11 21:19:15 +0600

    feat: add patch route to controller so it can handle image

--- server/controllers/goods.controller.js
+++ server/controllers/goods.controller.js
@@ -2,11 +2,11 @@ import Goods from "../models/Goods.js";
 
 export const createProduct = async (req, res) => {
     try {
-        const {title, category, description, price, rating} = req.body;
+        const { title, category, description, price, rating } = req.body;
         // req.file содержит загруженный файл
         // /uploads/${req.file.filename} — путь к изображению
         const image = req.file ? `/uploads/${req.file.filename}` : null;
-   
+
         const good = new Goods({
             title,
             category,
@@ -15,9 +15,40 @@ export const createProduct = async (req, res) => {
             rating,
             image //Сохраняем путь к файлу в БД
         });
-        
+
         await good.save();
         res.status(201).json(good);
+    } catch (e) {
+        res.status(500).json({
+            message: "Smth wrong, try again",
+            error: e.message,
+        });
+    }
+};
+
+export const patchProduct = async (req, res) => {
+    try {
+        console.log("BODY:", req.body);
+        console.log("FILE:", req.file);
+
+        const { id } = req.params;
+
+        const { title, category, description, price, rating, image } = req.body;
+
+        const good = await Goods.findById(id);
+        if (!good) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+        (good.title = title),
+            (good.category = category),
+            (good.description = description),
+            (good.price = price),
+            (good.rating = rating),
+            (good.image = image),
+
+            await good.save();
+        res.status(200).json(good);
     } catch (e) {
         res.status(500).json({
             message: "Smth wrong, try again",
--- server/routes/goods.routes.js
+++ server/routes/goods.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Goods from "../models/Goods.js";
-import { upload } from "../middleware/upload.js";
-import { createProduct } from "../controllers/goods.controller.js";
+import upload from "../middleware/upload.js";
+import { createProduct, patchProduct } from "../controllers/goods.controller.js";
 
 const router = Router();
 
@@ -42,46 +42,20 @@ router.get("/:id", async (req, res) => {
 });
 
 // /api/goods/:id
-router.patch("/:id", async (req, res) => {
-	try {
-		const { id } = req.params;
-
-		const {title, category, description, price, rating, image} = req.body;
-
-		const good = await Goods.findById(id);
-		if (!good) {
-			res.status(404).json({ message: "User not found" });
-			return;
-		}
-		(good.title = title),
-			(good.category = category),
-			(good.avatar = avatar),
-			(good.description = description),
-			(good.price = price),
-			(good.rating = rating),
-			(good.image = image),
-			
-			await good.save();
-		res.status(200).json(good);
-	} catch (e) {
-		res.status(500).json({
-			message: "Smth wrong, try again",
-			error: e.message,
-		});
-	}
-});
+router.patch("/:id", upload.single("image"), patchProduct);
 
 // /api/goods/:id
 router.delete("/:id", async (req, res) => {
     try {
         const { id } = req.params;
 
-        const good = await Goods.findById(id);
+		const good = await Goods.findByIdAndDelete(id);
+		
         if (!good) {
-            res.status(404).json({ message: "User not found" });
+            res.status(404).json({ message: "Product not found" });
             return;
         }
-        await good.remove();
+
         res.status(200).json({ message: "Good deleted" });
     } catch (e) {
         res.status(500).json({
@@ -92,25 +66,3 @@ router.delete("/:id", async (req, res) => {
 });
 
 router.post("/create", upload.single("image"), createProduct);
-// /api/goods/create
-// router.post("/create", async (req, res) => {
-//     try {
-//         const {title, category, description, price, rating, image} = req.body;
-
-//         const good = new Goods({
-//             title,
-//             category,
-//             description,
-//             price,
-//             rating,
-//             image
-//         });
-//         await good.save();
-//         res.status(201).json(good);
-//     } catch (e) {
-//         res.status(500).json({
-//             message: "Smth wrong, try again",
-//             error: e.message,
-//         });
-//     }
-// });
