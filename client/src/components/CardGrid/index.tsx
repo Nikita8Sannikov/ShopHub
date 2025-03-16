6cfb35388e612b4f2d55b4f9a694fdf392f22880
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-16 12:31:52 +0600

    feat: add card for adding product

--- client/src/components/CardGrid/index.tsx
+++ client/src/components/CardGrid/index.tsx
@@ -1,24 +1,48 @@
-import { Product } from '@/types/types';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { RootState } from '@/store';
+import { Product } from '@/types/types';
 
 interface CardGridProps {
-    items: Product[];
-    renderItem: (item: Product) => React.ReactNode;
+  items: Product[];
+  renderItem: (item: Product) => React.ReactNode;
 }
 
 const CardGrid = ({ items, renderItem }: CardGridProps) => {
+  const user = useSelector((state: RootState) => state.auth.user);
+const navigate = useNavigate();
 
+   const callbacks = {
+         onCreate: () => {
+             navigate("/create");
+         },
+        }
   return (
     <div className="user-cards">
-    { items.map((item: Product) => (
-         <div
-         key={item.id}
-         className="user-card"
-     >
-        { renderItem(item) }
+      {user?.isAdmin &&
+        <div className='user-card' onClick={ callbacks.onCreate}>
+          <img
+            // src={product.image}
+            alt={`+`}
+            className="avatar"
+          />
+          <h2 className="name">
+            Add new item
+          </h2>
+
         </div>
-     ))}
-</div>
+      }
+      {items.map((item: Product) => (
+        <div
+          key={item._id}
+          className="user-card"
+        >
+          {renderItem(item)}
+        </div>
+      ))}
+    </div>
   )
 }
 
