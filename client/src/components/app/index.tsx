3aeb756ee566769526363910b0ae4f8d0979aed1
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-03-20 15:53:44 +0600

    fix: remove custom modal elements

--- client/src/components/app/index.tsx
+++ client/src/components/app/index.tsx
@@ -5,13 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import useRoutes from "../../hooks/useRoutes";
 import { remind } from "../../store/reducers/auth/authSlice";
-import Basket from "../basket";
 import Spinner from "../spinner";
 
 function App() {
 	const dispatch: AppDispatch = useDispatch();
 	const status = useSelector((state: RootState) => state.auth.status);
-	const activeModal = useSelector((state: RootState) => state.modal.name);
 	const router = useRoutes();
 
 	useEffect(() => {
@@ -19,13 +17,12 @@ function App() {
 	}, [dispatch]);
 
 	if (status === "loading") {
-		return <Spinner/>;
+		return <Spinner />;
 	}
 
 	return (
 		<BrowserRouter>
 			{router}
-			{activeModal === "basket" ? <Basket /> : null}
 		</BrowserRouter>
 	);
 }
