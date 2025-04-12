76fa7dcb97144d8b89c174a52d2c0ac2d68d1061
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-12 17:34:59 +0600

    refactor: add cors to config

--- server/app.js
+++ server/app.js
@@ -9,19 +9,24 @@ import finalConfig from "./config/index.js";
 import cors from "cors";
 
 const app = express();
-const allowedOrigins = ["http://localhost:5002"];
+const allowedOrigins = finalConfig.allowedOrigins;
+
 app.use(cors({
-    origin: (origin, callback) => {
+	origin: (origin, callback) => {
 		console.log(origin);
-		
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
+
+		if (!origin || allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true
 })); // Включает CORS для всех запросов
+
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("Allowed Origins:", finalConfig.allowedOrigins);
+
 app.use(express.json()); // Миддлвар для обработки JSON-тел запросов
 app.use(express.urlencoded({ extended: true })); // Миддлвар для обработки URL-кодированных запросов
 app.use(cookieParser()); //Мидлвар cookie-parser разбирает cookies и делает их доступными через req.cookies
@@ -33,7 +38,7 @@ app.use("/api/cart", cartRoutes);
 //раздаём папку "uploads" как статические файлы
 app.use("/uploads", express.static("uploads"));
 
-const PORT = process.env.PORT || 5001
+const PORT = finalConfig.port|| 5001;
 
 async function start() {
 	try {
--- server/config/index.js
+++ server/config/index.js
@@ -10,11 +10,22 @@ dotenv.config();
 const defaultsPath = path.resolve("config/default.json");
 const defaults = JSON.parse(fs.readFileSync(defaultsPath, "utf8"));
 
+// Читаем конфиг для CORS
+const corsConfigPath = path.resolve("config/cors.json");
+const corsConfig = JSON.parse(fs.readFileSync(corsConfigPath, "utf8"));
+
+// Определяем разрешённые Origins в зависимости от среды
+const allowedOrigins =
+  process.env.NODE_ENV === "development"
+    ? corsConfig.allowedOrigins.development
+    : corsConfig.allowedOrigins.production;
+
 // Объединяем конфиги
 const finalConfig = {
     port: config.get("port") || defaults.port,
     mongoUri: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET,
+    allowedOrigins
 };
 
 export default finalConfig;
