5730fb3978075545c1868afa330be8dfa0cf2f68
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-17 21:38:20 +0600

    fix: fix sideLayout and add select component

--- client/src/components/sideLayout/index.tsx
+++ client/src/components/sideLayout/index.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import "./style.css";
+
 interface SideLayoutProps {
 	children: React.ReactNode;
 	side: "start" | "end" | "between";
 }
 const SideLayout: React.FC<SideLayoutProps> = ({ children, side }) => {
+	const sideClasses = {
+		start: "justify-start",
+		end: "justify-end",
+		between: "justify-between",
+	};
+
 	return (
-		<div className={`SideLayout_${side}`}>
+		<div className={`flex items-center ${sideClasses[side]}`}>
 			{React.Children.map(children, (child) => {
 				if (React.isValidElement(child)) {
 					return (
-						<div key={child?.key} className="SideLayout-item">
+						<div key={child?.key} className="mx-[5px]">
 							{child}
 						</div>
 					);
 				} else {
-					return <div className="SideLayout-item">{child}</div>;
+					return <div className="mx-[5px]">{child}</div>;
 				}
 			})}
 		</div>
