aff6f6480a504898eb010785fc095cb856102a05
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-04-27 13:10:12 +0600

    feat: add clear method to popover logout button

--- client/src/components/popoverAvatar/index.tsx
+++ client/src/components/popoverAvatar/index.tsx
@@ -1,3 +1,8 @@
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState } from "@/store"
+import { signOut } from "@/store/reducers/auth/authSlice"
+import { clearCart } from "@/store/reducers/cart/cartSlice"
+
 import { Button } from "@/components/ui/button"
 import {
     Popover,
@@ -5,19 +10,17 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
-import { useDispatch, useSelector } from "react-redux"
-import { AppDispatch, RootState } from "@/store"
-import { signOut } from "@/store/reducers/auth/authSlice"
 
 export function PopoverAvatar() {
     const dispatch: AppDispatch = useDispatch();
     const user = useSelector((state: RootState) => state.auth.user);
 
     const callbacks = {
-            onLogout: () => {
-                dispatch(signOut());
-            }
-        };
+        onLogout: () => {
+            dispatch(signOut());
+            dispatch(clearCart());
+        }
+    };
 
     return (
         <Popover>
@@ -27,23 +30,23 @@ export function PopoverAvatar() {
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
             </PopoverTrigger>
-        {user &&
-            <PopoverContent className="w-auto">
-                <div
-                    className="user-name"
-                >
+            {user &&
+                <PopoverContent className="w-auto">
+                    <div
+                        className="cursor-pointer underline"
+                    >
 
-                    {`Вы - `}
-                    {user?.name}{" "}
-               
-                </div>
-                <Button
-                    onClick={callbacks.onLogout}
-                >
-                    Выход
-                </Button>
-            </PopoverContent>
-}
+                        {`Вы - `}
+                        {user?.name}{" "}
+
+                    </div>
+                    <Button
+                        onClick={callbacks.onLogout}
+                    >
+                        Выход
+                    </Button>
+                </PopoverContent>
+            }
         </Popover>
     )
 }
